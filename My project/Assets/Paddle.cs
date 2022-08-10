7b737aab0bae012c6c66dffad53124375e88f930
Geogory <Mohammad.azem@ymail.com>
2022-08-10 19:47:44 +0300

    Created Brick Breaker

---
+++ My project/Assets/Paddle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    Rigidbody2D RB;
+
+    float PlayerInput;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerInput = Input.GetAxis("Horizontal");
+        RB.velocity = new Vector2(PlayerInput * speed, 0);
+    }
+}
